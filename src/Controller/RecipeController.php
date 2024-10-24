f2cfef3445dbc8c5ba0289d426e31857319a6b92
GitZak <zakaria.belrhali@gmail.com>
2024-10-24 15:32:15 +0100

    Repository Entity

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,17 +15,25 @@ class RecipeController extends AbstractController
     #[Route(
         path: '/recipe',
         name: 'recipe.index',
-    )]    public function index(Request $request): Response
+    )]    public function index(Request $request, RecipeRepository $recipeRepository, EntityManagerInterface $em): Response
     {
+
+        dd($recipeRepository->findTotalDuration());
+        
+        $data = $recipeRepository->findWithDurationLowerThen(10);
+        // $data[0]->setTitle('tajine 3anzi b bar9oo9');
+        // $em->flush();
         // return new Response(content: 'Recettes');
-        return $this->render('recipe/index.html.twig');
+        return $this->render('recipe/index.html.twig', [
+            'data' => $data
+        ]);
     }
 
     #[Route(
         path: '/recipe/{slug}-{id}',
         name: 'recipe.show',
         requirements: ['id' => '\d+', 'slug' => '[a-zA-Z0-9\-]+']
-    )]    public function show(Request $request, string $slug, int $id)
+    )]    public function show(Request $request, string $slug, int $id, RecipeRepository $recipeRepository)
     {
         // dd($request->attributes->get(key: 'id'), $request->attributes->get(key: 'slug'));
         // dd($slug, $id);
@@ -35,9 +45,17 @@ class RecipeController extends AbstractController
 
         // return new Response(content: 'Recette :' . $request->attributes->get(key: 'slug'));
 
-        
+        $recipe = $recipeRepository->find($id);
+
+        if($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', [
+                'slug' => $recipe->getSlug(),
+                'id' => $recipe->getId()
+            ]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
+            'recipe' => $recipe,
         ]);
     }
 }
